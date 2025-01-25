309ed6fc6d6b89987616cbd46e3bccb64d89adaa
Kalyan Momidi <kmomidi21@gmail.com>
2025-01-24 19:30:15 -0500

    Initial commit

---
+++ catalog-service/src/main/java/com/kalyan/catalog_service/domain/Product.java
@@ -0,0 +1,5 @@
+package com.kalyan.catalog_service.domain;
+
+import java.math.BigDecimal;
+
+public record Product(String code, String name, String description, String imageUrl, BigDecimal price) {}
